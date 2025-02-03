375f73c508515a9a7dd58da31c83b591992a19d7
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-02-03 13:21:09 -0300

    new7

---
+++ atividades prova/somaelementos.py
@@ -0,0 +1,9 @@
+#Crie uma função que receba uma tupla de números e retorne a soma de seus elementos.
+
+
+def soma_tupla(numeros):
+    return sum(numeros)
+    
+valores = (5,10,3,25,8)
+eu = soma_tupla(valores)
+print(eu)
