b4233bbc9459b2d6f6900894d83f3cbafadfda8b
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-02-02 22:41:58 -0300

    new4

---
+++ atividades prova/produto.py
@@ -0,0 +1,20 @@
+dic = {}
+
+while True:
+    tupla1 = input("Digite o nome do produto: ")
+    tupla2 = input("Digite a quantidade do produto: ")
+    dic[tupla1] = [tupla2]
+    sair = str(input("Digite (x) caso queira sair: "))
+    if sair in "xX":
+        break
+print(dic)
+
+def busca_preco(nome):
+    for c in dic:
+        if c == nome:
+            return dic[nome]
+        elif nome not in dic:
+            return "Item não existente"
+    
+eu = busca_preco("coca")
+print(eu)
