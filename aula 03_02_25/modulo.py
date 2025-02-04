ad7fd67cb0ba10785f1acb725f467305926b38aa
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-02-03 23:17:15 -0300

    03/02/25

---
+++ aula 03_02_25/modulo.py
@@ -0,0 +1,19 @@
+import matematica as mt
+
+while True:
+    n1 = int(input("Digite o primeiro numero a ser calculado: "))
+    n2 = int(input("Digite o segundo numero a ser calculado: "))
+    ope = str(input("Digite a operação desejada (+,-,* ou /): "))
+
+    match ope:
+        case("+"):
+            print(mt.soma(n1,n2))
+        
+        case("-"):
+            print(mt.sub(n1,n2))
+        
+        case("*"):
+            print(mt.multi(n1,n2))
+
+        case("/"):
+            print(mt.divi(n1,n2))
