271ed80faa2162cda38afa3d1da17e866368e898
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-02-07 20:16:55 -0300

    10

---
+++ atividades prova/desconto.py
@@ -0,0 +1,13 @@
+# Dado o seguinte dicionário de produtos e seus respectivos preços:
+# produtos = { "arroz": 10.0, "feijão": 8.5, "macarrão": 5.0, "carne": 35.0 }
+# Implemente uma função chamada aplica_desconto que receba o dicionário e um valor percentual de desconto e retorne um novo dicionário com os produtos e seus preços com o desconto aplicado
+
+produtos = { "arroz": 10.0, "feijão": 8.5, "macarrão": 5.0, "carne": 35.0 }
+
+def aplica_desconto(dicionario: str, desconto: int):
+    novoproduto = {}
+    for item, valor in dicionario.items():
+        novoproduto[item] = valor* desconto / 100 + valor
+    return novoproduto
+
+print(aplica_desconto(produtos, 100))
