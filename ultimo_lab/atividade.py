f48d6125802334767ce480bddc0998ae516d4247
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-02-21 19:44:21 -0300

    primeira parte pronta

--- ultimo_lab/atividade.py
+++ ultimo_lab/atividade.py
@@ -1,15 +1,19 @@
 # Dado o arquivo anexo com transações de uma conta bancária, faça um programa que leia o arquivo, calcule o saldo final da pessoa e o imprima na tela, depois salve o total por categoria em outro arquivo, numa subpasta no formato (YYYYMM), onde Y é o ano e M é o mês das transações.
 
 try:
-    with open("Júlio Kennedy dos Santos Silva - transacoes.csv")  as arquivo:
+    with open("Júlio Kennedy dos Santos Silva - transacoes.csv", "r")  as arquivo:
         texto = arquivo.readlines()
         valor = 0
-        for valores in texto:
+        lista = []
+        lista_atualizada = []
+        for valores in texto[1:]:
             novos = valores.strip().split(";")
-            novos[3] = int(novos[3])
-            novos[3] = float(novos[3])
-            novos[3] += valor
-        print(valor)
+            lista.append(novos[3])
+        for num in lista:
+            num2 = num.replace(",", ".")
+            num2 = float(num2)
+            lista_atualizada.append(num2)
+        print(f"Seu saldo final é: {sum(lista_atualizada)}")
 
         
 
